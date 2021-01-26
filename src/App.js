26450b1b550785ff7d5acf115701a4e12b0938ed
serleo <serleo96.98@gmail.com>
2021-01-26 20:22:57 -0300

    initial

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <nav class="navbar navbar-dark bg-dark">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Default</a>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default App;
